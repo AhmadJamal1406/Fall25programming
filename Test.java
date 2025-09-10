e8e862f73f3529e7d13fe1d57e25764e433a367c
AhmadJamal1406 <ahmadjamal140608@gmail.com>
2025-09-10 15:42:44 +0500

    Add files via upload

---
+++ Test.java
@@ -0,0 +1,19 @@
+//Sp25-Bcs-088
+//Muhammad Ahmad Jamal
+
+public class Test{
+
+
+	public static void main(String[] args){
+
+
+	Student s=new Student("Ahmad","Email",3.61);
+	s.Display();
+}
+
+
+
+
+
+
+}
